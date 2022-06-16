70aa804022fa2bbd0da307b986ccb0e1eef8f794
Alejandro Pozo <hap921020@gmail.com>
2022-06-16 12:28:28 -0500

    diseño entrega books

--- app/Views/pages/entrega.php
+++ app/Views/pages/entrega.php
@@ -15,7 +15,16 @@
     </div>
     <!-- end page title -->
     <div class="row shadow-lg p-3 mb-5 mt-4 bg-body rounded">
-        <h4 class="header-title"><button class="btn btn-warning ms-2" id="btn-upEntrega"><i class="mdi mdi-autorenew"></i></button><button class="btn btn-primary ms-2" id="btn-upBuscar"><i class="uil-search-alt"></i></button></h4>
+        <h4 class="header-title">Formulario</h4>
+
+        <p class="text-muted font-13">Lorem ipsum <code>{Breve descripcion del form}</code> dolor sit amet
+            consectetur adipisicing elit. Atque iusto cum, vel cupiditate quaerat modi quis porro dolores est
+            incidunt exercitationem quibusdam tempore repudiandae, enim deserunt dolorum eos excepturi rerum.
+            Aut, culpa mollitia hic quidem, vel ex veritatis assumenda vero minus repudiandae dolor inventore
+            accusamus deleniti cum placeat sapiente blanditiis dolorum expedita enim repellendus perspiciatis
+            quasi quae. Quia, accusamus commodi?</p>
+        <br>
+        <h4 class="page-title"><button class="btn btn-warning ms-2" id="btn-upEntrega"><i class="mdi mdi-autorenew"></i></button><button class="btn btn-primary ms-2" id="btn-upBuscar"><i class="uil-search-alt"></i></button></h4>
         <div class="mb-3 col-md-3 col-sm-12">
             <!-- Single Select -->
             <select class="form-control select2" data-toggle="select2" id="selectCI">
